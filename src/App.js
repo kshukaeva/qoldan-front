4a8120376edfad7bee12da9d8f33961bbe2264ce
kshukaeva <kshukaevaaa@gmail.com>
2023-05-13 14:55:04 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,170 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import {Routes, Route } from 'react-router-dom';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import ItemCard from "./components/ItemCard";
+import Products from "./components/AllProduct";
+import Cart from "./components/Cart";
+import Bookmark from './components/Bookmark';
+import UserProfile from './components/UserProfile';
+import Register from './components/Register';
+import AddProduct from './components/AddProduct';
+import ForgotPassword from "./components/ForgetPassword";
+
 
 function App() {
+  const [orders, setOrders] = useState([]);
+  const [favourites, setFavourites] = useState([]);
+  const [currentItems, setCurrentItems] = useState([]);
+  const [items] = useState([
+    {
+      "id": 1,
+      "title": "Women's Sweater",
+      "img": "clothes.jpg",
+      "desc": "Stay cozy with this stylish women's sweater. Made from soft and warm material, perfect for the colder months.",
+      "category": "clothing",
+      "price": "4999"
+    },
+    {
+      "id": 2,
+      "title": "Wireless Headphones",
+      "img": "instax.jpg",
+      "desc": "Enjoy your music on the go with these high-quality wireless headphones. Connect to any device via Bluetooth.",
+      "category": "electronics",
+      "price": "8999"
+    },
+    {
+      "id": 3,
+      "title": "Garden Gloves",
+      "img": "home.jpg",
+      "desc": "Protect your hands while gardening with these durable and comfortable gloves. Available in multiple sizes.",
+      "category": "home",
+      "price": "1299"
+    },
+    {
+      "id": 4,
+      "title": "Yoga Mat",
+      "img": "tennis.jpg",
+      "desc": "Get your yoga practice started with this high-quality yoga mat. Made from non-slip material for a safe and comfortable experience.",
+      "category": "sport",
+      "price": "2999"
+    },
+    {
+      "id": 5,
+      "title": "The Great Gatsby Book",
+      "img": "book.jpg",
+      "desc": "Experience the classic story of The Great Gatsby by F. Scott Fitzgerald. Perfect for any book lover.",
+      "category": "books",
+      "price": "999"
+    },
+    {
+      "id": 6,
+      "title": "Board Game Set",
+      "img": "toys.jpg",
+      "desc": "Enjoy hours of family fun with this board game set. Includes classic games like Monopoly and Scrabble.",
+      "category": "games",
+      "price": "3999"
+    },
+    {
+      "id": 7,
+      "title": "SUV",
+      "img": "bycycle.jpg",
+      "desc": "Experience the comfort and luxury of an SUV. Perfect for road trips and family vacations.",
+      "category": "vehicles",
+      "price": "39999"
+    },
+    {
+      "id": 8,
+      "title": "Men's T-Shirt",
+      "img": "clothes.JPG",
+      "desc": "Stay stylish with this classic men's t-shirt. Available in multiple colors and sizes.",
+      "category": "clothing",
+      "price": "1999"
+    },
+    {
+      "id": 9,
+      "title": "Smart Watch",
+      "img": "instax.JPG",
+      "desc": "Stay connected on the go with this high-tech smart watch. Features include fitness tracking and phone notifications.",
+      "category": "electronics",
+      "price": "14999"
+    },
+    {
+      "id": 10,
+      "title": "Kitchen Knife Set",
+      "img": "home.JPG",
+      "desc": "Upgrade your kitchen with this high-quality knife set. Made from durable materials and includes a knife block for storage.",
+      "category": "home",
+      "price": "7999"
+    }
+  ]);
+
+
+  useEffect(() => {
+    setCurrentItems(items);
+  }, [items]);
+
+  function chooseCategory(category) {
+    if (category === 'all') {
+      setCurrentItems(items);
+    } else {
+      setCurrentItems(items.filter((el) => el.category === category));
+    }
+  }
+
+  function deleteOrder(id) {
+    setOrders(orders.filter(el => el.id !== id));
+  }
+
+  function addToOrder(item) {
+    let isInArray = false;
+    orders.forEach(el => {
+      if (el.id === item.id) {
+        isInArray = true;
+      }
+    });
+    if (!isInArray) {
+      setOrders([...orders, item]);
+    }
+  }
+
+   function deleteFavourites(id) {
+    setFavourites(favourites.filter(el => el.id !== id));
+  }
+
+  function addToFavourites(item) {
+    let isInArray = false;
+    favourites.forEach(el => {
+      if (el.id === item.id) {
+        isInArray = true;
+      }
+    });
+    if (!isInArray) {
+      setFavourites([...favourites, item]);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Header/>
+      <main>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home currentItems={items} onAdd={addToOrder} addFavourites={addToFavourites} deleteFavourites={deleteFavourites}/>} />
+          <Route path="/all" element={<Products currentItems={currentItems} onAdd={addToOrder} addFavourites={addToFavourites} chooseCategory={chooseCategory} deleteFavourites={deleteFavourites}/>} />
+          <Route path="/item/:id" element={<ItemCard items={items} onAdd={addToOrder} addFavourites={addToFavourites} deleteFavourites={deleteFavourites}  />} />
+          <Route path="/cart" element={<Cart items={items} orders={orders} onDelete={deleteOrder} />} />
+          <Route path="/fav" element={<Bookmark items={items} favourites={favourites} onAdd={addToOrder} addFavourites={addToFavourites} deleteFavourites={deleteFavourites} />} />
+          <Route path="/user-profile" element={<UserProfile/>}/>
+          <Route path="/add-product" element={<AddProduct/>}/>
+          <Route path="/forget-password" element={<ForgotPassword/>}/>
+        </Routes>
+      </main>
+      <Footer/>
+      </div>
   );
 }
 
